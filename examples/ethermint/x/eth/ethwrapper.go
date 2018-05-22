51de673fe2de8d1ebd229bb9e29f016a1d34c647
David Kajpust <kajpustd@gmail.com>
2018-05-22 20:18:55 -0300

    first commit

--- examples/ethermint/x/eth/ethwrapper.go
+++ examples/ethermint/x/eth/ethwrapper.go
@@ -4,23 +4,23 @@ import (
 	"math/big"
 
 	eth_common "github.com/ethereum/go-ethereum/common"
-	eth_params "github.com/ethereum/go-ethereum/params"
 	eth_core "github.com/ethereum/go-ethereum/core"
+	eth_params "github.com/ethereum/go-ethereum/params"
 	//eth_types "github.com/ethereum/go-ethereum/core/types"
 )
 
 var DefaultChainConfig = &eth_params.ChainConfig{
-		ChainId:             big.NewInt(1),
-		HomesteadBlock:      big.NewInt(1),
-		DAOForkBlock:        big.NewInt(1),
-		DAOForkSupport:      false,
-		EIP150Block:         big.NewInt(1),
-		EIP150Hash:          eth_common.Hash{}, // Optional feature
-		EIP155Block:         big.NewInt(1),
-		EIP158Block:         big.NewInt(1),
-		ByzantiumBlock:      big.NewInt(1),
-		ConstantinopleBlock: nil,
-		Ethash:              nil,
+	ChainId:             big.NewInt(1),
+	HomesteadBlock:      big.NewInt(1),
+	DAOForkBlock:        big.NewInt(1),
+	DAOForkSupport:      false,
+	EIP150Block:         big.NewInt(1),
+	EIP150Hash:          eth_common.Hash{}, // Optional feature
+	EIP155Block:         big.NewInt(1),
+	EIP158Block:         big.NewInt(1),
+	ByzantiumBlock:      big.NewInt(1),
+	ConstantinopleBlock: nil,
+	Ethash:              nil,
 }
 
 var DefaultGenesis = &eth_core.Genesis{
